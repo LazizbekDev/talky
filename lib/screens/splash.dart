98285b22ba12755dcb6fd797aaaf3629f32b5756
LazizbekDev <lazizbektojiboyev0611@gmail.com>
2024-09-26 19:01:11 +0500

    optimized splash screen

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,10 +1,7 @@
 import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-// import 'package:talky/screens/home.dart';
 import 'package:talky/screens/set_profile.dart';
-// import 'package:talky/screens/home.dart';
-// import 'package:talky/screens/set_profile.dart';
 import 'package:talky/screens/user_entry.dart';
 import 'package:talky/utilities/app_colors.dart';
 import 'package:talky/widgets/logo.dart';
@@ -19,32 +16,25 @@ class Splash extends StatelessWidget {
       nextScreen: _nextScreen(),
       backgroundColor: AppColors.primaryColor,
       childWidget: const Center(
-        child: Logo(
-          dark: false,
-        ),
+        child: Logo(dark: false),
       ),
     );
   }
 
   Widget _nextScreen() {
-    return StreamBuilder(
+    return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (ctx, snapshot) {
+      builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return FlutterSplashScreen.fadeIn(
+          return const Scaffold(
             backgroundColor: AppColors.primaryColor,
-            childWidget: const Center(
-              child: Logo(
-                dark: false,
-              ),
+            body: Center(
+              child: Logo(dark: false),
             ),
           );
         }
 
-        if (snapshot.hasData) {
-          return const SetProfile();
-        }
-        return const UserEntry();
+        return snapshot.hasData ? const SetProfile() : const UserEntry();
       },
     );
   }
